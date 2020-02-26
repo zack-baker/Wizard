f7d102cef6a26ab6b9135cc6b7e7a5041481eaf1
Zack Baker <zack.baker@protonmail.ch>
2020-02-26 00:09:38 -0700

    Created tests for Player classes

---
+++ src/test/groovy/PlayerTest.groovy
@@ -0,0 +1,23 @@
+package io.zackb.wizard.test
+
+import io.zackb.wizard.HumanPlayer
+import io.zackb.wizard.main.AiPlayer
+
+class PlayerTest extends GroovyTestCase {
+    void 'test create HumanPlayer with parent constructor'(){
+        HumanPlayer hp = new HumanPlayer("Greg")
+        assert hp instanceof HumanPlayer
+    }
+    void 'test create AiPlayer with parent constructor'(){
+        AiPlayer ai = new AiPlayer("RB-88")
+        assert ai instanceof AiPlayer
+    }
+    void 'test HumanPlayer is human'(){
+        HumanPlayer hp = new HumanPlayer()
+        assert hp.isHuman()
+    }
+    void 'test AiPlayer is AI'(){
+        AiPlayer ai = new AiPlayer()
+        assert ai.isAi()
+    }
+}
