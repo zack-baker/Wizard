1459ce4252074b0dadc24649c2b3766e42873e1b
Zack Baker <zack.baker@protonmail.ch>
2020-02-27 23:21:01 -0700

    Refactored tests to use new RoundScore constructor

--- src/test/groovy/io/zackb/wizard/test/RoundScoreTest.groovy
+++ src/test/groovy/io/zackb/wizard/test/RoundScoreTest.groovy
@@ -5,19 +5,19 @@ import io.zackb.wizard.data.RoundScore
 class RoundScoreTest extends GroovyTestCase{
     void 'test bid matching trick earns points'(){
         for(int bid=0;bid<10;bid++){
-            RoundScore rs = new RoundScore(bid, bid, 1)
+            RoundScore rs = new RoundScore(bid, 1, bid)
             assert rs.getScore() == 20 + 10*bid
         }
     }
     void 'test mismatched bid and tricks won loses points'(){
         int tricksWon = 11
         for(int bid=0;bid<tricksWon;bid++){
-            RoundScore rs = new RoundScore(bid, tricksWon, 1)
+            RoundScore rs = new RoundScore(bid, 1, tricksWon)
             assert rs.getScore() == -10*(tricksWon-bid)
         }
         tricksWon = 0
         for(int bid = 10; bid>0; bid--){
-            RoundScore rs = new RoundScore(bid, tricksWon, 1)
+            RoundScore rs = new RoundScore(bid, 1, tricksWon)
             assert rs.getScore() == -10*(bid-tricksWon)
         }
     }
