ff865359b5b3a6ec22be16cf40f1d9918188fbcc
Zack Baker <zack.baker@protonmail.ch>
2020-02-27 23:20:46 -0700

    Added tests for scoring

--- src/test/groovy/io/zackb/wizard/test/PlayerTest.groovy
+++ src/test/groovy/io/zackb/wizard/test/PlayerTest.groovy
@@ -2,6 +2,7 @@ package io.zackb.wizard.test
 
 import io.zackb.wizard.HumanPlayer
 import io.zackb.wizard.AiPlayer
+import io.zackb.wizard.data.RoundScore
 
 class PlayerTest extends GroovyTestCase {
     void 'test create HumanPlayer with parent constructor'(){
@@ -13,11 +14,28 @@ class PlayerTest extends GroovyTestCase {
         assert ai instanceof AiPlayer
     }
     void 'test HumanPlayer is human'(){
-        HumanPlayer hp = new HumanPlayer("Name")
+        HumanPlayer hp = new HumanPlayer("Nick")
         assert hp.isHuman()
     }
     void 'test AiPlayer is AI'(){
         AiPlayer ai = new AiPlayer()
         assert ai.isAi()
+    }
+    void 'test get total scores'(){
+        HumanPlayer hp = new HumanPlayer("Tonto")
+        hp.scores = [new RoundScore(1,1,1),new RoundScore(2,2,0), new RoundScore(1,3,2)]
+        assert hp.getTotalScore() == 0
+        AiPlayer ai = new AiPlayer("Sentinel")
+        ai.scores = [new RoundScore(1,1,1), new RoundScore(2,2,2), new RoundScore(3,3,3)]
+        assert ai.getTotalScore() == 120
+
+    }
+    void 'test increment player score'(){
+        HumanPlayer hp = new HumanPlayer("Ryan")
+        int roundNumber = 1
+        hp.scores << new RoundScore(3,roundNumber)
+        hp.increaseScore(1)
+        assert hp.scores.find{it.roundNumber==roundNumber}.tricksWon == 1
+
     }
 }
