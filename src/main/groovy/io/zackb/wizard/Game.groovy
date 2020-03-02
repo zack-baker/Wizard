3c92442787ecdea7bfd7f3df5ed00462883eeb77
Zack Baker <zack.baker@protonmail.ch>
2020-03-01 23:01:43 -0700

    Added custom number of rounds field to Game object

--- src/main/groovy/io/zackb/wizard/Game.groovy
+++ src/main/groovy/io/zackb/wizard/Game.groovy
@@ -5,6 +5,7 @@ import io.zackb.wizard.data.Deck
 class Game {
     List<Player> players
     Deck deck
+    int numRounds
 
     /**
      * Default constructor for the Game. Game objects
@@ -14,8 +15,15 @@ class Game {
      * @param players the list of players in the game
      * @param deck the deck to be used in the game
      */
-    Game(List<Player> players, Deck deck){
+    Game(List<Player> players, Deck deck, int numRounds){
         this.players = players
         this.deck = deck
+        this.numRounds = numRounds
+    }
+
+    @Override
+    /** Overridden toString method. Prints player names */
+    String toString() {
+        return players.collect{it.name}.join(", ")
     }
 }
