1391f64d7dd5ad924c6305f5a851696c222dbd4c
Zack Baker <zack.baker@protonmail.ch>
2020-03-01 23:03:58 -0700

    Created playCard method for human players to play cards

--- src/main/groovy/io/zackb/wizard/HumanPlayer.groovy
+++ src/main/groovy/io/zackb/wizard/HumanPlayer.groovy
@@ -1,6 +1,8 @@
 package io.zackb.wizard
 
+import io.zackb.wizard.data.Card
 import io.zackb.wizard.data.RoundScore
+import io.zackb.wizard.enums.Suit
 
 class HumanPlayer extends Player{
 
@@ -14,6 +16,7 @@ class HumanPlayer extends Player{
      * @param roundNumber
      */
     void makeBid(int roundNumber) {
+        println("Making bid for ${name}")
         RoundScore existingScore = scores.find{it.roundNumber == roundNumber}
         if(existingScore){
             println("Existing bid exists for round ${roundNumber}. Bid is ${existingScore.bid}")
@@ -21,12 +24,40 @@ class HumanPlayer extends Player{
         }
 
         int bid = -1
-        println("${this}'s hand: ${this.printHand()}")
-        while(bid < 0 && bid > roundNumber){
+        println("${this}'s hand: ")
+        printHand()
+        while(bid < 0 || bid > roundNumber){
             println("Make bid [0-${roundNumber}]: ")
             bid = System.console().readLine() as int
         }
         RoundScore rs = new RoundScore(bid, roundNumber)
         scores << rs
+    }
+
+
+    Card playCard(Suit leadSuit) {
+        int chosenCardIndex = 0
+        Card chosenCard = null
+        while(chosenCardIndex < 1 || chosenCardIndex > hand.size()) {
+            println("${name}, play a card: ")
+            printHand()
+            chosenCardIndex = System.console().readLine() as int
+            chosenCard = hand[chosenCardIndex-1]
+            if(!chosenCard){
+                chosenCardIndex = 0
+                continue
+            }
+
+            List handLeadSuitCards = hand.findAll{it.isPlayingCard() && it.suit == leadSuit}
+            if((chosenCard.isPlayingCard() && chosenCard.suit != leadSuit) && handLeadSuitCards){
+                println("If playing a playing card, you must play a card of the leading suit (${leadSuit}). You may play a wizard or jester at any time")
+                chosenCardIndex = 0
+            }
+        }
+        hand.remove(chosenCard)
+        return chosenCard
+
+
+
     }
 }
